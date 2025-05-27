d2e9491a28eda1dd67414ac65362530f3840c93b
OmRevankar <revankarom26@gmail.com>
2025-05-27 09:59:59 +0530

    Basic Server Customization , db connection

---
+++ BACKEND/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants,js"
+
+const connectDB = async function(){
+
+    try {
+        
+        const result = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log("Database Connected Successfully :",result.connection.host);
+
+    } catch (error) {
+        console.log("Database Connection Failure !");
+    }
+
+}
+
+export default connectDB;
--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,12 +1,19 @@
 import dotenv from "dotenv"
-import express from "express"
+import app from "./app.js"
+import connectDB from "./db/index.js"
 
 dotenv.config({
-    path : "./.env"
+    path : "../.env"
 })
 
-const app = express();
+connectDB()
+.then( () => {
 
-app.listen(process.env.PORT || 5000 , () => {
-    console.log("Server running on port :",process.env.PORT);
-})
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log("Server listening on port :",process.env.PORT);
+    })
+
+} )
+.catch( (error) => {
+    console.log("No Running Server - Invalid DB Connection ",error);
+} )
